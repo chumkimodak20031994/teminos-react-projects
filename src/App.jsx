e02ce1d483b7f0eda5c882d2291b86234cfd004f
chumki <chumkimodak900@gmail.com>
2023-08-09 11:24:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar4 from './Component/Navbar4/Navbar4';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar4></Navbar4>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Component/Repeat/Repeat.jsx
@@ -0,0 +1,225 @@
+import React from 'react';
+
+const Repeat = () => {
+    return (
+        <div>
+
+            <div>
+                <div className='bg-light'>
+                    <nav className="navbar bg-body-tertiary text-dark">
+                        <div className="container-fluid">
+                            <a className="navbar-brand">Temple University</a>
+                            <form className="d-flex" role="search">
+                                <input className="form-control me-2 p-2 " type="search" placeholder="Search Dosen" aria-label="Search"></input>
+                                <img className='pics' src="/assete/Rectangle.svg" alt="Bootstrap" width="30" height="24"></img>
+                            </form>
+                        </div>
+                    </nav>
+                </div>
+
+
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <ul className='home'>
+                            <a className="nav-link disabled" aria-disabled="true">program/</a>
+                            <a className="nav-link active" aria-current="page" href="#">
+                                <span className='home2'>First Year Student</span>
+                            </a>
+
+                        </ul>
+                    </div>
+                </nav>
+
+
+                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                    <div className="container-fluid">
+                        <span className="navbar-text">First Year Student</span>
+
+                        <div>
+                            <ul className="navbar-nav">
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Overview</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Insights</a>
+                                </li>
+                                <li className="nav-item nav nav nav-underline">
+                                    <a className="nav-link active" aria-current="page" href="#"><span className='red'>Users</span></a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Matches</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link active" aria-current="page" href="#">Setting</a>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+
+                <div className="card text-bg-light m-0">
+                    <div className='btn-cls'>
+                        <button type="button" className="btn-close float-end m-4" aria-label="Close"></button>
+                    </div>
+                </div>
+                <div className='m-0 p-2'>
+                    <div class="row">
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+
+                        </div>
+                        <div className="card text-bg-light">
+                            <img src="/assete/headerbg.svg" className="card-img" alt="..."></img>
+                            <div className="card-img-overlay d-flex">
+                                <h5 className="card-title">
+                                    <img className='pic' src="/assete/Rectangle (1).svg" alt="Bootstrap" width="50" height="64"></img>
+                                </h5>
+
+                                <p className="card-text m-4 w-full"><span className='card-tex'>Jonathan Javelle</span>
+                                    <span><p className='jon'>Jonathan @ gmail.com +1 123-456-789</p></span>
+                                </p>
+
+
+
+                                <a><button className='m-4 br-4 text-primary border border-primary'>student</button></a>
+
+                                <a href="/src/Component/Repeat/Repeat.jsx" className='button-set'> <button type="button" className="btn btn-primary h-20  w-70">Public Profile</button></a>
+                            </div>
+                        </div>
+                        <div className="card text-center m-0 bg-white">
+                            <ul className="nav nav-pills card-header-pills">
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Major</a>
+                                    <span>Digital Media</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">year</a>
+                                    <span>2016</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Country</a>
+                                    <span>Ireland</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Role</a>
+                                    <span>Mentee</span>
+                                </li>
+
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Gender</a>
+                                    <span>Male</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Dob</a>
+                                    <span>04/13/1985</span>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                        <div className='card text-center m-0 bg-white m-0'>
+                            <ul className="nav nav-pills m-0 h-25 bg-white">
+
+                                <li className="nav-item">
+                                    <h4>Pograms</h4>
+                                    <div className="button">
+                                        <button type="button" className="btn btn-primary g-col-6 m-2">First year Student</button>
+                                        <button type="button" className="btn btn-primary rounded-pill">mba</button>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="card text-center bg-white m-2">
+                        <div>
+                            <h4 className='profile'>profile infirmation</h4>
+                        </div>
+
+                        <div className="card-header bg-white">
+
+                            <ul className="nav nav-pills card-header-pills">
+
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Studebt number</a>
+                                    <span>10123</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Deperment</a>
+                                    <span>Design</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Year</a>
+                                    <span>2016</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">User Type</a>
+                                    <span>Student</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">College</a>
+                                    <span>College of arts and science</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Minor</a>
+                                    <span>Computer Science</span>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link disabled" aria-disabled="true">Length of course</a>
+                                    <span>4 years</span>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="card text-center M-5">
+                            <div className="card-header bg-white">
+                                <ul className="nav nav-pills card-header-pills">
+                                    <li className="nav-item">
+                                        <a className="nav-link disabled" aria-disabled="true">pogram types</a>
+                                        <span>Carear</span>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link disabled" aria-disabled="true">onbording status</a>
+                                        <span>invites</span>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link disabled" aria-disabled="true">Linkedin url
+                                        </a>
+                                        <span>
+                                            <img className='pic' src="/assete/link.png" alt="Bootstrap" width="5" height="5"></img>
+                                        </span>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link disabled" aria-disabled="true">Dosen Url</a>
+                                        <img className='pic' src="/assete/image 14 (1).svg" alt="Bootstrap" width="10" height="10"></img>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+            {/* <div class="col-sm-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div> */}
+
+
+
+
+        </div>
+
+
+    );
+};
+
+export default Repeat;
